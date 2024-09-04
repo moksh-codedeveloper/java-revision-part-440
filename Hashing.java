f959f88d51f1909aa5990c8e3d355efc6836140b
MokshMalde <maldemoksh437@gmail.com>
2024-09-04 08:48:43 +0530

    first commit

---
+++ Hashing.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hashing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashMap <Integer, Integer> map = new HashMap<>();
+        map.put(122, 22323);
+        map.put(343, 45656);
+        map.put(565, 67878);
+        map.put(787, 89090);
+        System.out.println(map);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# hashing of java
+In this data structure you are going to learn about how to insert search and remove elements in pairs of two same or different data types 
